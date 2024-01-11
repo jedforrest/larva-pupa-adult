4476c7e87098bd74b147e9b520e306ef4d708e2c
Joshua Forrest <jedforrest@gmail.com>
2024-01-12 00:13:23 +1100

    WIP homotopy solver

--- julia/LPA_simulations.jl
+++ julia/LPA_simulations.jl
@@ -12,12 +12,11 @@ function LPA(du, u, p, t)
     du[1] = b * A * exp(-cel * L - cea * A)
     du[2] = (1 - μl) * L
     du[3] = P * exp(-cpa * A) + (1 - μa) * A
-    return du
 end
 
 function generate_synthetic_data(model, u0, params; steps=10)
     prob = DiscreteProblem(model, u0, (0., steps), params)
-    sol = solve(prob, FunctionMap())
+    sol = OrdinaryDiffEq.solve(prob, FunctionMap())
     sol[:,:]
 end
 
@@ -46,6 +45,6 @@ sample_ICs(lb, ub) = Float64.(rand(DiscreteUniform(lb, ub)))
 sampled_u0 = [sample_ICs(225, 275), sample_ICs(0, 10), sample_ICs(80, 120)]
 
 # Generates a matrix of values for LPA at t = 0, 1, ..., N
-N = 10
+N = 40
 LPAdata = generate_synthetic_data(LPA, sampled_u0, sampled_params; steps=N)
 LPAdata[:,1:4]
