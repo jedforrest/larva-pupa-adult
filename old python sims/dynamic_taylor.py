bc33114ad06e854ba7928baa635f4e6714f62a9a
Matias Bender <matiasbender@dhcp-12-106.institut.math.upmc.fr>
2024-01-16 18:06:53 +0100

    generate system compatible with julia

--- old python sims/dynamic_taylor.py
+++ old python sims/dynamic_taylor.py
@@ -13,6 +13,8 @@ from homotopy_continuation import solve
 import lpa_params
 from simulation import simulate
 
+from random import random
+
 DEBUG=False
 
 def factorial(n):
@@ -40,39 +42,25 @@ def debug(*args, color=33):
     if DEBUG:
         print(f"\033[{color};1m", *args, "\033[0m")
 
-def larva_poly_system(degree, adult_pop_series, larva_pop_series, b_interval, c_el_interval, c_ea_interval):
+def larva_poly_system_withapprox(degree, adult_pop_series, larva_pop_series, c_el_approx, c_ea_approx):
     b, c_el, c_ea = symbols(f"b c_el c_ea")
-    b_min, b_max = b_interval
-    c_el_min, c_el_max = c_el_interval
-    c_ea_min, c_ea_max = c_ea_interval
     assert len(adult_pop_series) == len(larva_pop_series), "Adult and larva population time series should have same length."
     polynomials = []
     for i, (a, l) in enumerate(zip(adult_pop_series, larva_pop_series)):
         if i == len(adult_pop_series)-1:
             break
-        exp_input_min = -(c_el_max*l + c_ea_max*a)
-        exp_input_max = -(c_el_min*l + c_ea_min*a)
-        exp_input_middle = (exp_input_max + exp_input_min) / 2
-        exp_approx = taylor_approx(degree, exp_input_middle)
+        exp_input = - (c_el_approx*l + c_ea_approx*a)
+        exp_approx = taylor_approx(degree, exp_input)
         l_next = larva_pop_series[i+1]
         poly = expand(b * a * exp_approx(-(c_el * l + c_ea * a)) - l_next)
         polynomials.append(poly)
-        debug("basepoint range = ", exp_input_min, exp_input_max, color=32)
+        debug("basepoint approx = ", exp_input, color=32)
         debug("poly approx error = ", poly.subs(b, lpa_params.b).subs(c_el, lpa_params.c_el).subs(c_ea, lpa_params.c_ea), color=33)
     return polynomials
 
-### def pupa_poly_system(degree, larva_pop_series, pupa_pop_system, mu_l_interval):
-###     mu_l = symbols(f"mu_l")
-###     assert len(larva_pop_series) == len(pupa_pop_system)
-###     for i, (l, p) in enumerate(zip(larva_pop_series, pupa_pop_system)):
-###         if i == len(larva_pop_series)-1
-###     # TODO
-###     pass
 
-
-def adult_poly_system(degree, adult_pop_series, pupa_pop_series, c_pa_interval, mu_a_interval):
+def adult_poly_system_withapprox(degree, adult_pop_series, pupa_pop_series, c_pa_approx):
     c_pa, mu_a = symbols(f"c_pa mu_a")
-    c_pa_min, c_pa_max = c_pa_interval
     #mu_a_min, mu_a_max = mu_a_interval
     assert len(adult_pop_series) == len(pupa_pop_series), "Adult and pupa population time series should have same length."
     polynomials = []
@@ -80,57 +68,56 @@ def adult_poly_system(degree, adult_pop_series, pupa_pop_series, c_pa_interval, 
         if i == len(adult_pop_series)-1:
             break
         # a_new = a * (1 - mu_a) + p * exp(-(c_pa * a))
-        exp_input_min = -(c_pa_max*p)
-        exp_input_max = -(c_pa_min*p)
-        exp_input_middle = (exp_input_max + exp_input_min) / 2
-        exp_approx = taylor_approx(degree, exp_input_middle)
+        exp_input = -(c_pa_approx*p)
+        exp_approx = taylor_approx(degree, exp_input)
         a_next = adult_pop_series[i+1]
         poly = expand(a*(1-mu_a) + p * exp_approx(-c_pa*p) - a_next)
         polynomials.append(poly)
-        debug("basepoint range = ", exp_input_min, exp_input_max, color=32)
+        debug("basepoint approx = ", exp_input, color=32)
         debug("poly approx error = ", poly.subs(mu_a, lpa_params.mu_a).subs(c_pa, lpa_params.c_pa), color=33)
     return polynomials
 
 
+### The default values of off make sense for the default values in lpa_params. We should update this.
+def gensys(prolog, degree, off=[0.0001,0.0001,0.0001]):
+        L, P, A = simulate (prolog+1)
+        lpoly = larva_poly_system_withapprox(degree, A, L, lpa_params.c_el+off[0], lpa_params.c_ea+off[1])
 
 
-if __name__ == "__main__":
-    from random import random
-    for degree in range(1, 10):
-        print(('degree', degree))
-        default_interval_min = lambda: 0.757 + random()/100
-        default_interval_max = lambda: 1.22 + random()/100
-        default_intervals = {
-                "b":    [default_interval_min()*lpa_params.b,    default_interval_max()*lpa_params.b],
-                "c_el": [default_interval_min()*lpa_params.c_el, default_interval_max()*lpa_params.c_el],
-                "c_ea": [default_interval_min()*lpa_params.c_ea, default_interval_max()*lpa_params.c_ea],
-                "c_pa": [default_interval_min()*lpa_params.c_pa, default_interval_max()*lpa_params.c_pa],
-                "mu_a": [default_interval_min()*lpa_params.mu_a, default_interval_max()*lpa_params.mu_a],
-        }
-        L, P, A = simulate(4)
-        print(repr("larva poly system"))
-        lpoly = larva_poly_system(degree, A, L,
-                b_interval=default_intervals["b"],
-                c_el_interval=default_intervals["c_el"],
-                c_ea_interval=default_intervals["c_ea"])
-        polynomials = []
+        print("howoff = {};".format(off))
+        
+        truesolLarva = "solL = [{} ; {} ; {}];".format(lpa_params.c_el,lpa_params.c_ea,lpa_params.b)
+
+        print(truesolLarva)
+        
+        mysys = "fL = ["
         for poly in lpoly:
-            polynomials.append(pretty_poly(poly))
-        print(repr(("True-solution", {"b": lpa_params.b, "c_el": lpa_params.c_el, "c_ea": lpa_params.c_ea})))
-        start_time = time()
-        for i, solution in enumerate(solve(["b", "c_el", "c_ea"], polynomials)):
-            print(repr((i, solution)))
-        print(("total-time", time() - start_time))
+            strpoly = str(poly)
+            strpoly = strpoly.replace("**","^")
+            strpoly = strpoly.replace("c_el","c[1]")
+            strpoly = strpoly.replace("c_ea","c[2]")
+            mysys +=  strpoly.replace("b","c[3]")
+            mysys += "; "
 
-        apoly = adult_poly_system(degree, A, P,
-                c_pa_interval=default_intervals["c_pa"],
-                mu_a_interval=default_intervals["mu_a"])
-        print("adult poly system")
-        polynomials = []
-        for poly in apoly[:2]:
-            polynomials.append(pretty_poly(poly))
-        print("True solution", {"c_pa": lpa_params.c_pa, "mu_a": lpa_params.mu_a})
-        start_time = time()
-        for i, solution in enumerate(solve(["c_pa", "mu_a"], polynomials)):
-            print(repr((i, solution)))
-        print(("total-time", time() - start_time))
+        mysys = mysys[:-3]
+        mysys += "];"
+        print(mysys)
+            
+        
+        apoly = adult_poly_system_withapprox(degree, A, P, lpa_params.c_pa+off[2])
+
+        truesolAdult = "solA = [{} ; {}];".format(lpa_params.c_pa,lpa_params.mu_a)
+
+        print(truesolAdult)
+        
+        mysys = "fA = ["
+        for poly in apoly:
+            strpoly = str(poly)
+            strpoly = strpoly.replace("**","^")
+            strpoly = strpoly.replace("c_pa","d[1]")
+            mysys +=  strpoly.replace("mu_a","d[2]")
+            mysys += "; "
+
+        mysys = mysys[:-3]
+        mysys += "];"
+        print(mysys)
